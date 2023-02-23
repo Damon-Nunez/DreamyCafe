9421c52c3602c4f65b4fa9ad993721ad8fa10c57
Damon <damon.j.nunez@gmail.com>
2023-02-22 21:19:00 -0500

    Page Not Rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import React from 'react';
+import Home from './pages/Home';
+import { Menu } from './pages/Menu';
+import { AboutUs } from './pages/AboutUs';
+import { Books } from './pages/Books';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+  Navigate,
+  Switch,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-     <h1> Lol </h1>
-    </div>
+  <Router>
+    <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route exact path='/AboutUs' element={<AboutUs/>}/>
+    <Route exact path='/Books' element={<Books/>}/>
+    <Route exact path ='/Menu' element={<Menu/>}/>
+    </Routes>
+  </Router>
   );
 }
 
